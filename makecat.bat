3c26b5378e21453e1177ba9997e4b7cee1a10efe
Lucas Walter <wsacul@gmail.com>
2017-06-22 16:16:53 -0700

    Don't need path variable, fix it in dosbox.conf

--- makecat.bat
+++ makecat.bat
@@ -1,5 +1,9 @@
-set tpath=C:\other\retro\dos\TURBOP
-%tpath%\TASM\tasm catasm
-%tpath%\TASM\tasm catasm
-%tpath%\TASM\tasm soundlib
-%tpath%\TP\tpc -b catacomb
+REM set tpath=C:\other\retro\dos\TURBOP
+REM %tpath%\TASM\tasm src\catasm
+REM Need to put this in dosbox.conf
+REM MOUNT C ~
+REM PATH=%PATH%;C:\other\retro\dos\TURBOP\TP;C:\other\retro\dos\TURBOP\TASM
+REM
+tasm src\catasm
+tasm src\soundlib
+tpc -b src\catacomb
